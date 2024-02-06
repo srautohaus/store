bc8cff64fa5b8738460dc6c7561395d0c94f1896
Suteemon <suteemon.team@newton.ac.th>
2024-02-06 23:31:11 +0700

    test branch

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -15,11 +15,8 @@ export default function NotFound() {
       <p className="text-small-regular text-ui-fg-base">
         The page you tried to access does not exist.
       </p>
-      <Link
-        className="flex gap-x-1 items-center group"
-        href="/"
-      >
-        <Text className="text-ui-fg-interactive">Go to frontpage</Text>
+      <Link className="flex gap-x-1 items-center group" href="/">
+        <Text className="text-ui-fg-interactive">Go to homepage</Text>
         <ArrowUpRightMini
           className="group-hover:rotate-45 ease-in-out duration-150"
           color="var(--fg-interactive)"
